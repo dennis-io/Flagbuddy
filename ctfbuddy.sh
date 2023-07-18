eae60de06255ee2a499213eb89f00be4695e3411
neutronsec <dhermanns@pm.me>
2023-07-18 13:01:09 +0200

    requirements.txt

--- ctfbuddy.sh
+++ ctfbuddy.sh
@@ -1,41 +1,57 @@
 #!/bin/bash
 # ctfbuddy.sh
 
-# Get current working directory
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# Move to the home directory
+cd ~
 
-# Check if required tools are installed and install them if not
-for tool in git python3 pip curl; do
-    command -v $tool >/dev/null 2>&1 || { echo >&2 "$tool is required but it's not installed. Installing now..."; sudo apt-get install -y $tool; }
-done
+# Clone the GitHub repository
+git clone https://github.com/neutronsec/ctfbuddy
 
-# Clone the repository
-git clone https://github.com/neutronsec/ctfbuddy.git
-
-# Go to the cloned repository directory
+# Move into the cloned directory
 cd ctfbuddy
 
-# Create a python virtual environment
+# Check for Python 3 and pip3 installation
+if ! command -v python3 &> /dev/null
+then
+    echo "Python3 could not be found. Installing now..."
+    sudo apt-get install -y python3
+fi
+
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 could not be found. Installing now..."
+    sudo apt-get install -y python3-pip
+fi
+
+# Check for venv module in Python3
+python3 -c "help('modules venv')" > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Python3 venv module not found. Installing python3-venv now..."
+    sudo apt-get install -y python3-venv
+fi
+
+# Create a Python virtual environment
 python3 -m venv venv
 
 # Activate the virtual environment
 source venv/bin/activate
 
-# Install Python dependencies (replace 'requirements.txt' with actual filename if different)
-pip install -r requirements.txt
+# Install required Python libraries
+pip3 install python-nmap
 
-# Create a bash script to make the python script globally runnable
-echo "#!/bin/bash
-source $SCRIPT_DIR/ctfbuddy/venv/bin/activate
-python3 $SCRIPT_DIR/ctfbuddy/buddy.py" > ctfbuddy.sh
+# Make the script executable
+chmod +x buddy.py
 
-# Move the bash script to /usr/local/bin/ and make it executable
-sudo cp ctfbuddy.sh /usr/local/bin/ctfbuddy
-sudo chmod +x /usr/local/bin/ctfbuddy
+# Add an alias to .bashrc or .zshrc for the `ctfbuddy` command
+if [[ "$SHELL" == *"zsh"* ]]; then
+    echo "alias ctfbuddy='`pwd`/buddy.py'" >> ~/.zshrc
+    source ~/.zshrc
+else
+    echo "alias ctfbuddy='`pwd`/buddy.py'" >> ~/.bashrc
+    source ~/.bashrc
+fi
 
-# Install system-wide dependencies
-sudo apt-get update
-sudo apt-get install -y nmap hydra
+# Deactivate the virtual environment
+deactivate
 
-# Let the user know that the setup has finished
-echo "ctfbuddy setup completed. You can now run 'ctfbuddy' from anywhere."
+echo "CTFBuddy is set up! You can now use it by typing 'ctfbuddy' in the terminal."
